53aa71f4b668194d79a6cd0370fb082627ea680f
fe3dback <fe3dback@protonmail.com>
2023-05-17 20:50:28 +0300

    add temporary filter3

--- ecs/filter.go
+++ ecs/filter.go
@@ -16,12 +16,20 @@ type (
 		include []bits.Id
 		exclude []bits.Id
 	}
+	filter3[T1, T2, T3 Component] struct {
+		world   *World
+		include []bits.Id
+		exclude []bits.Id
+	}
 
 	Filter1[T1 Component] interface {
 		Find() Result1[T1]
 	}
 	Filter2[T1, T2 Component] interface {
 		Find() Result2[T1, T2]
+	}
+	Filter3[T1, T2, T3 Component] interface {
+		Find() Result3[T1, T2, T3]
 	}
 )
 
@@ -51,6 +59,27 @@ func NewFilter2[T1, T2 Component](w RuntimeWorld, exclude ...Component) Filter2[
 		include: []bits.Id{
 			w.(*World).registry.bitsRegistry.ComponentBits(string(TType1.TypeID())),
 			w.(*World).registry.bitsRegistry.ComponentBits(string(TType2.TypeID())),
+		},
+		exclude: make([]bits.Id, len(exclude)),
+	}
+
+	for ind, excludeCmp := range exclude {
+		flt.exclude[ind] = w.(*World).registry.bitsRegistry.ComponentBits(string(excludeCmp.TypeID()))
+	}
+
+	return flt
+}
+func NewFilter3[T1, T2, T3 Component](w RuntimeWorld, exclude ...Component) Filter3[T1, T2, T3] {
+	var TType1 T1
+	var TType2 T2
+	var TType3 T3
+
+	flt := &filter3[T1, T2, T3]{
+		world: w.(*World),
+		include: []bits.Id{
+			w.(*World).registry.bitsRegistry.ComponentBits(string(TType1.TypeID())),
+			w.(*World).registry.bitsRegistry.ComponentBits(string(TType2.TypeID())),
+			w.(*World).registry.bitsRegistry.ComponentBits(string(TType3.TypeID())),
 		},
 		exclude: make([]bits.Id, len(exclude)),
 	}
@@ -112,6 +141,42 @@ func (flt *filter2[T1, T2]) Find() Result2[T1, T2] {
 		result.ent = append(result.ent, ent)
 		result.cmp1 = append(result.cmp1, cmp1.(any).(*T1))
 		result.cmp2 = append(result.cmp2, cmp2.(any).(*T2))
+	}
+
+	return result
+}
+func (flt *filter3[T1, T2, T3]) Find() Result3[T1, T2, T3] {
+	var TType1 T1
+	var TType2 T2
+	var TType3 T3
+	result := &filterResult3[T1, T2, T3]{}
+
+	for _, ent := range flt.world.entities.Iterate() {
+		if !ent.componentMask.ContainAll(flt.include...) {
+			continue
+		}
+
+		if ent.componentMask.ContainAny(flt.exclude...) {
+			continue
+		}
+
+		cmp1, exist := ent.components.Get(TType1.TypeID())
+		if !exist {
+			continue
+		}
+		cmp2, exist := ent.components.Get(TType2.TypeID())
+		if !exist {
+			continue
+		}
+		cmp3, exist := ent.components.Get(TType3.TypeID())
+		if !exist {
+			continue
+		}
+
+		result.ent = append(result.ent, ent)
+		result.cmp1 = append(result.cmp1, cmp1.(any).(*T1))
+		result.cmp2 = append(result.cmp2, cmp2.(any).(*T2))
+		result.cmp3 = append(result.cmp3, cmp3.(any).(*T3))
 	}
 
 	return result
--- ecs/filter_results.go
+++ ecs/filter_results.go
@@ -12,6 +12,13 @@ type (
 		cmp2 []*T2
 		iter int
 	}
+	filterResult3[T1, T2, T3 any] struct {
+		ent  []*Entity
+		cmp1 []*T1
+		cmp2 []*T2
+		cmp3 []*T3
+		iter int
+	}
 
 	Result1[T1 any] interface {
 		Next() bool
@@ -47,10 +54,21 @@ func (q *filterResult2[T1, T2]) Next() bool {
 	q.iter++
 	return true
 }
+func (q *filterResult3[T1, T2, T3]) Next() bool {
+	if q.iter >= len(q.ent) {
+		return false
+	}
+
+	q.iter++
+	return true
+}
 
 func (q *filterResult1[T1]) Get() (*Entity, *T1) {
 	return q.ent[q.iter-1], q.cmp1[q.iter-1]
 }
 func (q *filterResult2[T1, T2]) Get() (*Entity, *T1, *T2) {
 	return q.ent[q.iter-1], q.cmp1[q.iter-1], q.cmp2[q.iter-1]
+}
+func (q *filterResult3[T1, T2, T3]) Get() (*Entity, *T1, *T2, *T3) {
+	return q.ent[q.iter-1], q.cmp1[q.iter-1], q.cmp2[q.iter-1], q.cmp3[q.iter-1]
 }
